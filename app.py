7662d241bf17c372343909866332db3614eb2053
Rayyan Majid <rayyanmajidis@gmail.com>
2021-08-03 03:29:48 -0400

    added secret key config

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ if __name__ == '__main__':
     from views import views
 
     app.register_blueprint(views, url_prefix='/')
+    app.config['SECRET_KEY'] = "THIS IS MY SECRET KEY"
 
     app.run(debug=True)
