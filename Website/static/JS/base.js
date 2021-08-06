a384cb425226ff7bfc7b2616388147b2fc8f3490
Rayyan Majid <rayyanmajidis@gmail.com>
2021-08-06 04:12:32 -0400

    added image in sidebar, added about page, fixed javascript

--- Website/static/JS/base.js
+++ Website/static/JS/base.js
@@ -5,11 +5,13 @@ function toggleSidebar() {
     console.log("opening sidebar");
     document.getElementById("mySidebar").style.width = "300px";
     document.getElementById("main").style.marginLeft = "300px";
+    document.getElementById("suit").style.opacity = "1";
     this.mini = false;
   } else {
     console.log("closing sidebar");
-    document.getElementById("mySidebar").style.width = "85px";
-    document.getElementById("main").style.marginLeft = "85px";
+    document.getElementById("mySidebar").style.width = "80px";
+    document.getElementById("main").style.marginLeft = "80px";
+    document.getElementById("suit").style.opacity = "0";
     this.mini = true;
   }
 }
