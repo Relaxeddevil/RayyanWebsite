4f1b6c2ef957c6614b7bac8e2eeaaadf63adf64c
Rayyan Majid <rayyanmajidis@gmail.com>
2021-08-21 01:35:23 -0400

    adding specific chrome user argument

--- Amazon/__init__.py
+++ Amazon/__init__.py
@@ -24,7 +24,7 @@ def get_soup(search_term):
 
     options.add_argument("--headless")
     options.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                       "Chrome/92.0.4515.131 ""Safari/537.36")
+                         "Chrome/92.0.4515.131 ""Safari/537.36")
 
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
