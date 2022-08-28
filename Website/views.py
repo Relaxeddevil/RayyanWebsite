e734dddd261c96a38834d19ca18f6ef3617377fb
Rayyan Majid <rayyanmajidis@gmail.com>
2022-08-28 05:36:27 -0400

    fixed message flash

--- Website/views.py
+++ Website/views.py
@@ -29,7 +29,7 @@ def projects():
 def amazon():
     if request.method == "POST":
         input = request.form.get('search')
-        flash('Searching for your results, please provide a few minutes while I scrape', 'info')
+        flash('Searching for your results, please provide a few minutes while I scrape')
         df = create_dataframe(input)
         create_bp(df)
         save_to_excel(df)
